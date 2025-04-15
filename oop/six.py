6dc29f02ef4f9314d9cccc06dae9e2bfd85d4924
govindabhandara <gvndbhandara@gmail.com>
2025-04-15 13:02:39 +0530

    first commit

---
+++ oop/six.py
@@ -0,0 +1,13 @@
+class Account:
+    '''claass created by govinda'''
+
+    def open_account(self):
+        pass
+    def deposit_amount(self,amount):
+        pass 
+    @classmethod
+    def update_min_bal(cls,amount):
+        pass 
+    @staticmethod
+    def cal_interest():
+        pass
