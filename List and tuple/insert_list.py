bcf26f5af53e417376440db3673b9eefbfcad4ea
govindabhandara <gvndbhandara@gmail.com>
2025-05-23 23:50:24 +0530

    list

---
+++ List and tuple/insert_list.py
@@ -0,0 +1,4 @@
+numbers = [1, 2, 3]
+numbers.insert(1, 10)  # Insert 10 at index 1
+numbers.sort()
+print(numbers)  # Output: [1, 10, 2, 3]
--- List and tuple/prac.py
+++ List and tuple/prac.py
@@ -1,2 +1,3 @@
-number=[1,2,3,4,5,6,7,8,9,10]
-if 
+numbers=[1,2,3,4]
+numbers.insert(6,5)
+print(numbers)
